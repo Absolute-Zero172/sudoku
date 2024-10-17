74458625c6667df35bb60220bc7194c574fef63a
Absolute-Zero172 <calvinjwasilevich@gmail.com>
2024-10-17 11:30:35 -0500

    added options functions

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from random import choice
 
 
 class Board:
@@ -54,7 +55,7 @@ class Board:
         c.sort()
         return c == [i for i in range(1, 10)]
 
-    def check_block(self, block_index):
+    def is_block_complete(self, block_index):
         block_list = []
         xst = 0
         yst = 0
@@ -78,3 +79,51 @@ class Board:
         block_list.sort()
 
         return block_list == [i for i in range(1, 10)]
+
+    def options_for_position(self, x, y):
+        ideal = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        row_index = y
+        column_index = x
+        block_index = (x // 3) + (3 * (y // 3))
+
+        row = self.board[row_index]
+        column = [self.board[i][column_index] for i in range(9)]
+
+        block_list = []
+        xst = 0
+        yst = 0
+        if 0 <= block_index <= 2:
+            x_index = block_index
+            xst = x_index * 3
+            yst = 0
+        elif 3 <= block_index <= 5:
+            x_index = block_index - 3
+            xst = x_index * 3
+            yst = 3
+        elif 6 <= block_index <= 8:
+            x_index = block_index - 6
+            xst = x_index * 3
+            yst = 6
+
+        for y in range(3):
+            for x in range(3):
+                block_list.append(self.board[yst + y][xst + x])
+
+        taken = []
+        for item in row:
+            if item != 0 and item not in taken:
+                taken.append(item)
+                ideal.pop(ideal.index(item))
+        for item in column:
+            if item != 0 and item not in taken:
+                taken.append(item)
+                ideal.pop(ideal.index(item))
+        for item in block_list:
+            if item != 0 and item not in taken:
+                taken.append(item)
+                ideal.pop(ideal.index(item))
+
+        return ideal
+
+    def get_random_option(self, x, y):
+        return choice(self.options_for_position(x, y))
